1f1730dde128a3adc64509114f81cd3a026a05d6
Jack Grahl <jack.grahl@gmail.com>
2019-07-03 19:19:03 +0000

    Basic stuff.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyLDB',
+    version='0.1.0',
+    url='https://github.com/jwg4/pyldb',
+    author='Jack Grahl',
+    author_email='jack.grahl@gmail.com',
+    description='Retrieve and format data from the Live Departure Board',
+    packages=find_packages(),    
+    install_requires=['zeep'],
+)
