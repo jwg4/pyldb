6caf52b16828bedec9132d587936fd0f95135f92
Jack Grahl <jack.grahl@gmail.com>
2019-07-04 22:00:52 +0000

    Change where we write the sample to.

--- sample/get_board.py
+++ sample/get_board.py
@@ -6,5 +6,5 @@ from pyldb import get_board, render_board
 if __name__ == '__main__':
     db = get_board("NWD", token)
     output = render_board(db)
-    with open("board.html", 'w') as f:
+    with open(r"sample\output\board.html", 'w') as f:
         f.write(output)
