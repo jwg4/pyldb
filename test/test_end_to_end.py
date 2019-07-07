93fef74438dbf8c65668d0429c53890fb55ccd24
Jack Grahl <jack.grahl@gmail.com>
2019-07-07 22:42:19 +0000

    typo render -> render_board

--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -2,12 +2,12 @@ import os
 
 from unittest import TestCase
 
-from pyldb import get_board, render
+from pyldb import get_board, render_board
 
 
 class TestRender(TestCase):
     def test_do_a_render(self):
         token = os.environ["PYLDB_API_TOKEN"]
-        board = get_board("VIC", token)
-        html = render(board)
+        data = get_board("VIC", token)
+        html = render_board(data)
         self.assertIsNotNone(html)
