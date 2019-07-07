5af4eb449448b42e51bab7d260a861e32da7bc57
Jack Grahl <jack.grahl@gmail.com>
2019-07-07 23:22:59 +0000

    Add a comment, put back the token from env.

--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -9,8 +9,15 @@ from pyldb import get_board, render_board
 
 class TestRender(TestCase):
     def test_do_a_render(self):
-        #token = os.environ["PYLDB_API_TOKEN"]
-        from sample.credentials import token
+        """
+            This tst depends on two external services:
+            the LDBWS service itself, and the W3C HTML
+            validator service. So there should be at least
+            some expectation that it might fail because
+            of problems with or problems connecting to
+            those services, even if no code has changed.
+        """
+        token = os.environ["PYLDB_API_TOKEN"]
         data = get_board("VIC", token)
         html = render_board(data)
         self.validate(html)
