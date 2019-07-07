bf4128bd7fb9f2c5f304d9f30513dcda7db54ed5
Jack Grahl <jack.grahl@gmail.com>
2019-07-07 22:31:41 +0000

    Flake8 fixes.

--- pyldb/render.py
+++ pyldb/render.py
@@ -1,7 +1,5 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from .retrieve import get_board
-
 env = Environment(
     loader=FileSystemLoader('templates'),
     autoescape=select_autoescape(['html', 'xml'])
--- pyldb/retrieve.py
+++ pyldb/retrieve.py
@@ -6,15 +6,15 @@ from .details import wsdl_url, service_url
 def get_board(crs, token):
     """
         Get a live departure board from the LDBWS service.
-        The data shows the next 20 trains currently scheduled 
+        The data shows the next 20 trains currently scheduled
         to leave from that station. The data should match
         that shown on the real-life departure boards in the station
         and in mapping apps, etc.
 
          - crs: a three-letter token identifying a UK station.
-                You can find out the CRS of a station at 
+                You can find out the CRS of a station at
                 http://www.nationalrail.co.uk/stations_destinations/48541.aspx
-         - token: An API access token. 
+         - token: An API access token.
     """
     client = Client(wsdl_url, location=service_url)
 
