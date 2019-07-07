ab7c1c40e1ea1d3e7a96f01a8f0abc0e03cffcac
Jack Grahl <jack.grahl@gmail.com>
2019-07-07 22:10:55 +0000

    A basic test that we can retrieve data and render a template.

---
+++ test/test_end_to_end.py
@@ -0,0 +1,13 @@
+import sys
+
+from unittest import TestCase
+
+from pyldb import get_board, render
+
+
+class TestRender(TestCase):
+    def test_do_a_render(self):
+        token = sys.environ("PYLDB_API_TOKEN")
+        board = get_board("VIC", token)
+        html = render(board)
+        self.assertIsNotNone(html)
