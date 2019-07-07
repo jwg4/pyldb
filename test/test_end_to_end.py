1ed8ccb1347f52b1aada1bea67a7a9642354d42a
Jack Grahl <jack.grahl@gmail.com>
2019-07-07 22:37:47 +0000

    typo round to square brackets.

--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -7,7 +7,7 @@ from pyldb import get_board, render
 
 class TestRender(TestCase):
     def test_do_a_render(self):
-        token = os.environ("PYLDB_API_TOKEN")
+        token = os.environ["PYLDB_API_TOKEN"]
         board = get_board("VIC", token)
         html = render(board)
         self.assertIsNotNone(html)
