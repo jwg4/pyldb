8e049c1fe4106c974202c0fb40662aa1c2963bd9
Jack Grahl <jack.grahl@gmail.com>
2019-07-10 20:21:38 +0000

    Try to install the built dist.

--- test/test_script.sh
+++ test/test_script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+python setup.py sdist
+
 mkdir temp
 cp -R test/ temp
+cp dist/pyLDB-*.tar.gz temp
 
 cd temp/
 virtualenv venv
 . venv/bin/activate
 
-pip install ../
+pip install pyLDB-*.tar.gz
 python -m unittest discover test
