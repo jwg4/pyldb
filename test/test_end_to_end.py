a3be4a9c6c51f6f059c8c54f2e284daa0c237ece
Jack Grahl <jack.grahl@gmail.com>
2020-04-18 01:50:03 +0000

    Make it work if there aren't any trains.

--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -7,21 +7,7 @@ import requests
 from pyldb import get_board, render_board
 
 
-class TestRender(TestCase):
-    def test_do_a_render(self):
-        """
-            This tst depends on two external services:
-            the LDBWS service itself, and the W3C HTML
-            validator service. So there should be at least
-            some expectation that it might fail because
-            of problems with or problems connecting to
-            those services, even if no code has changed.
-        """
-        token = os.environ["PYLDB_API_TOKEN"]
-        data = get_board("VIC", token)
-        html = render_board(data)
-        self.validate(html)
-
+class TestHTMLOutput(TestCase):
     def validate(self, html):
         self.assertIsNotNone(html)
 
@@ -35,3 +21,32 @@ class TestRender(TestCase):
         messages = result['messages']
         errors = [ m for m in messages if m['type'] == 'error' ]
         self.assertListEqual([], errors)
+
+
+class TestRetrieveAndRender(TestHTMLOutput):
+    def test_do_a_render(self):
+        """
+            This test depends on two external services:
+            the LDBWS service itself, and the W3C HTML
+            validator service. So there should be at least
+            some expectation that it might fail because
+            of problems with or problems connecting to
+            those services, even if no code has changed.
+        """
+        token = os.environ["PYLDB_API_TOKEN"]
+        data = get_board("VIC", token)
+        html = render_board(data)
+        self.validate(html)
+
+
+class TestRender(TestHTMLOutput):
+    class StationBoardStub(object):
+        crs = "CLJ"
+        locationName = "Clapham Junction"
+        generatedAt = "2020-04-18T02:33:50.6760206+01:00"
+
+    def test_render_board_without_trains(self):
+        data = b'<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body><GetDepartureBoardResponse xmlns="http://thalesgroup.com/RTTI/2017-10-01/ldb/"><GetStationBoardResult xmlns:lt="http://thalesgroup.com/RTTI/2012-01-13/ldb/types" xmlns:lt6="http://thalesgroup.com/RTTI/2017-02-02/ldb/types" xmlns:lt7="http://thalesgroup.com/RTTI/2017-10-01/ldb/types" xmlns:lt4="http://thalesgroup.com/RTTI/2015-11-27/ldb/types" xmlns:lt5="http://thalesgroup.com/RTTI/2016-02-16/ldb/types" xmlns:lt2="http://thalesgroup.com/RTTI/2014-02-20/ldb/types" xmlns:lt3="http://thalesgroup.com/RTTI/2015-05-14/ldb/types"><lt4:generatedAt>2020-04-18T02:33:50.6760206+01:00</lt4:generatedAt><lt4:locationName>Norwood Junction</lt4:locationName><lt4:crs>NWD</lt4:crs><lt4:nrccMessages><lt:message>Reduced timetable in operation. In line with government advice, you should only be travelling if you are a key worker or it is essential for you to do so. If you must travel, always practice social distancing. More details can be found &lt;A href="http://nationalrail.co.uk/service_disruptions/245388.aspx"&gt;here&lt;/A&gt;.</lt:message><lt:message>Amended trains in the Guildford area. More details can be found &lt;A href="http://nationalrail.co.uk/service_disruptions/240366.aspx"&gt;here&lt;/A&gt;.</lt:message></lt4:nrccMessages><lt4:platformAvailable>true</lt4:platformAvailable></GetStationBoardResult></GetDepartureBoardResponse></soap:Body></soap:Envelope>'
+        data = TestRender.StationBoardStub()
+        html = render_board(data)
+        self.validate(html)
