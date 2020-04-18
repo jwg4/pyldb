09a46c6004165a1937f9f9c794172048da7b2044
Jack Grahl <jack.grahl@gmail.com>
2020-04-18 02:14:25 +0000

    bump version number

--- test/test_errors.py
+++ test/test_errors.py
@@ -8,4 +8,4 @@ from pyldb import get_board
 class TestRetrieveAndRender(TestCase):
     def test_bad_token(self):
         token = "blah-blah-blah-ASDF-QWERTY"
-        self.assertRaises(Exception, lambda : get_board("CLJ", token))
+        self.assertRaises(Exception, lambda: get_board("CLJ", token))
