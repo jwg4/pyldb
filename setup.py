510faf4da4d20b3c3cd63ffdc8c22c1923adba2c
Jack Grahl <jack.grahl@gmail.com>
2023-04-01 23:32:14 +0100

    Update version and deps.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyLDB',
-    version='0.5.0',
+    version='0.6.0',
     url='https://github.com/jwg4/pyldb',
     author='Jack Grahl',
     author_email='jack.grahl@gmail.com',
@@ -15,7 +15,7 @@ setup(
         "HTML departure board with configurable style and layout."
     ),
     packages=find_packages(),
-    install_requires=['suds-py3==1.3.3.0', 'jinja2==2.10.1'],
+    install_requires=['suds-py3==1.3.3.0', 'jinja2==3.1.2'],
     tests_require=['requests'],
     include_package_data=True,
     package_data={'': ['templates/*.j2', 'pyldb/extra/css/*.css']},
